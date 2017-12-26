b0256a574b1685df82ab00c0856aa73c5e698ed9
Thulio Philipe <thulioph@gmail.com>
2017-12-26 11:19:06 -0300

    Updates the search method to receive query and type as a parameter.

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -44,6 +44,23 @@ describe('Spotify Wrapper', () => {
       const artists = search();
 
       expect(fetchedStub).to.have.been.calledOnce;
+
+      fetchedStub.restore(); // close the stub
+    });
+
+    it('should receive the correct url to fetch.', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search('Incubus', 'artist');
+
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+      const albums = search('Incubus', 'albums');
+
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=albums');
+
+      fetchedStub.restore();
     });
   });
 });
