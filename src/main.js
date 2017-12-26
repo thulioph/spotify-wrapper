7037553e739fc12c82d81927fdd9ae4b173b762b
Thulio Philipe <thulioph@gmail.com>
2017-12-26 13:31:25 -0300

    Updates the search method with headers.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 /* global fetch:false */
 
-export const search = (query, type) => fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`).then(data => data.json());
+import CONFIG from '../src/config';
+
+export const search = (query, type) => {
+  const url = `https://api.spotify.com/v1/search?q=${query}&type=${type}`;
+  const options = {
+    headers: {
+      Authorization: CONFIG.Authorization,
+    },
+  };
+
+  return fetch(url, options).then(data => data.json());
+};
 
 export const searchAlbums = query => search(query, 'album');
 
