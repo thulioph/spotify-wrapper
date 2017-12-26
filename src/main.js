be26c013aabad34917dc5a49dd72a9b1ca26c993
Thulio Philipe <thulioph@gmail.com>
2017-12-26 10:52:10 -0300

    Adds the initial smoke test and methods.

---
+++ src/main.js
@@ -0,0 +1,9 @@
+export const search = () => {};
+
+export const searchAlbums = () => {};
+
+export const searchArtists = () => {};
+
+export const searchTracks = () => {};
+
+export const searchPlaylists = () => {};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,28 @@
-const { expect } = require('chai');
+import { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
-describe('Main', () => {
-  let arr;
+describe('Spotify Wrapper', () => {
+
+  context('Smoke Tests', () => {
+    it('should exist the search method.', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbums method.', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtists method.', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method.', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method.', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+
 });
