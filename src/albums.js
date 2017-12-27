f899682f0c8be6d112a3f35b23841b7402656f23
Thulio Philipe <thulioph@gmail.com>
2017-12-27 11:18:50 -0300

    Adds the album.js to manage the request for albums.

---
+++ src/albums.js
@@ -0,0 +1,8 @@
+/* global fetch:false */
+
+export const getAlbum = id => (
+  fetch(`https://api.spotify.com/v1/albums/${id}`)
+    .then(data => data.json())
+);
+
+export const getAlbumTracks = () => {};
