1889633e67908ccc8304a0a25d8745ab81aad040
Thulio Philipe <thulioph@gmail.com>
2017-12-28 18:03:07 -0300

    Removes the utils file.

--- src/album.js
+++ src/album.js
@@ -1,16 +1,15 @@
 /* global fetch:false */
 
 import { API_URL } from './config';
-import { toJSON } from './utils';
 
 export const getAlbum = id => (
-  fetch(`${API_URL}/albums/${id}`).then(data => toJSON(data))
+  fetch(`${API_URL}/albums/${id}`).then(data => data.json())
 );
 
 export const getAlbums = id => (
-  fetch(`${API_URL}/albums/?ids=${id}`).then(data => toJSON(data))
+  fetch(`${API_URL}/albums/?ids=${id}`).then(data => data.json())
 );
 
 export const getAlbumTracks = id => (
-  fetch(`${API_URL}/albums/${id}/tracks`).then(data => toJSON(data))
+  fetch(`${API_URL}/albums/${id}/tracks`).then(data => data.json())
 );
