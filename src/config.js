e51f472d513d50520caae6650d3195d6f2560087
Thulio Philipe <thulioph@gmail.com>
2017-12-29 16:52:32 -0300

    Removes the API_TOKEN from config file.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,1 @@
-export const CONFIG = {
-  Authorization: 'Bearer BQAIzfyCEHcG6SgwMGMXqWHIdJLajCox6WOino6rqAsxx9RJTSPbVUbBUCuvp2NmFemTY9riCIfNL4zvsYY',
-};
-
 export const API_URL = 'https://api.spotify.com/v1';
